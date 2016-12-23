c9743d0b48a4a21a9a335cad22c567fdcdfc6753
Nikita Nikitin <nikita-pupov@inbox.ru>
2016-12-23 19:22:04 +0200

    Added new func int Products

---
+++ Recommend.cpp
@@ -0,0 +1,19 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+
+#include "Products.h"
+#include "Main.h"
+#include "Meals.h"
+#include "Recommend.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TForm4 *Form4;
+//---------------------------------------------------------------------------
+__fastcall TForm4::TForm4(TComponent* Owner)
+	: TForm(Owner)
+{
+}
+//---------------------------------------------------------------------------
